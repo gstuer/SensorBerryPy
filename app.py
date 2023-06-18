c71881707380b47a13a0f6d73bd3f610a1edc76c
Moritz Gstuer <moritz@gstuer.com>
2023-06-18 11:22:19 +0200

    Add threading to sensor reading refresh

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT
 from flask import Flask, request, jsonify
 import time
+import threading
 
 SENSOR_TYPE = Adafruit_DHT.AM2302
 SENSOR_PIN = 2
@@ -8,36 +9,40 @@ SENSOR_READ_PAUSE = 15
 
 app = Flask(__name__)
 readCache = None
+readAttempts = list()
 
 def readSensor():
     global readCache
-    if readCache is not None:
-        cacheTimestamp, cachedHumidity, cachedTemperature = readCache
-    else:
-        cacheTimestamp = time.time() - 2 * SENSOR_READ_PAUSE
-    currentTime = time.time()
-    if cacheTimestamp + SENSOR_READ_PAUSE <= currentTime:
+    global readAttempts
+    while True:
+        currentTime = time.time()
+        print("Read attempt at " + str(currentTime) + "...")
         humidity, temperature = Adafruit_DHT.read_retry(SENSOR_TYPE, SENSOR_PIN)
         # Reading the sensor may take a couple of seconds -> Time needs to be refreshed.
         currentTime = time.time()
+        print("Finished at " + str(currentTime) + ": ")
         if humidity is not None and temperature is not None:
+            print("Successful")
             readCache = (currentTime, humidity, temperature)
-    return readCache
+        else:
+            print("Unsuccessful")
+        time.sleep(SENSOR_READ_PAUSE)
 
 @app.route("/sensors/temperature", methods=["GET"])
 def getTemperature():
-    measurement = readSensor()
-    if measurement is not None:
-        timestamp, humidity, temperature = measurement
+    if readCache is not None:
+        timestamp, humidity, temperature = readCache
         return jsonify({"value": temperature, "unit": "°C", "timestamp": timestamp}), 200
     else:
         return jsonify({"error": "Reading sensor failed"}), 500
 
 @app.route("/sensors/humidity", methods=["GET"])
 def getHumidity():
-    measurement = readSensor()
-    if measurement is not None:
-        timestamp, humidity, temperature = measurement
+    if readCache is not None:
+        timestamp, humidity, temperature = readCache
         return jsonify({"value": humidity, "unit": "%RH", "timestamp": timestamp}), 200
     else:
         return jsonify({"error": "Reading sensor failed"}), 500
+
+sensorThread = threading.Thread(target=readSensor)
+sensorThread.start()
