29418e1c6952c31d43d4f5bf0fcbf402c121b1fb
Moritz Gstuer <moritz@gstuer.com>
2023-11-18 20:23:06 +0100

    Add aggregates to get measurements endpoint

--- rest.py
+++ rest.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from repository import Repository
+from statistics import fmean
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -10,19 +11,49 @@ def getIndex():
 
 @app.route("/measurements", methods=["GET"])
 def getMeasurements():
-    repository = Repository()
+    # Get request arguments from url
     maxAge = float(request.args.get("max_age")) if request.args.get("max_age") is not None else None
     minAge = float(request.args.get("min_age")) if request.args.get("min_age") is not None else None
+
+    # Initialize repository and fetch rows
+    repository = Repository()
     rows = repository.findMeasurementsWithAge(minimum = minAge, maximum = maxAge)
 
-    # Add rows as dictionary to List
+    # Add rows as dictionary to list
     measurements = list()
     for row in rows:
         # Get values from row
-        timestamp, temperature, humidity, carbon_dioxide = row
-        measurement = {"timestamp": timestamp, "temperature": temperature, "humidity": humidity, "carbon_dioxide": carbon_dioxide}
+        timestamp, temperature, humidity, carbonDioxide = row
+        measurement = {"timestamp": timestamp, "temperature": temperature, "humidity": humidity, "carbon_dioxide": carbonDioxide}
         measurements.append(measurement)
-    return jsonify(measurements), 200
+
+    # Calculate aggregate functions for data
+    response = {}
+    temperatureValues = [measurement["temperature"] for measurement in measurements]
+    temperatureAggregates = {
+        "maximum": max(temperatureValues),
+        "mean_arithmetic": fmean(temperatureValues),
+        "minimum": min(temperatureValues)
+    }
+    humidityValues = [measurement["humidity"] for measurement in measurements]
+    humidityAggregates = {
+        "maximum": max(humidityValues),
+        "mean_arithmetic": fmean(humidityValues),
+        "minimum": min(humidityValues)
+    }
+    carbonDioxideValues = [measurement["carbon_dioxide"] for measurement in measurements]
+    carbonDioxideAggregates = {
+        "maximum": max(carbonDioxideValues),
+        "mean_arithmetic": fmean(carbonDioxideValues),
+        "minimum": min(carbonDioxideValues)
+    }
+
+    # Construct http response object
+    aggregates = {"temperature": temperatureAggregates, "humidity": humidityAggregates, "carbon_dioxide": carbonDioxideAggregates}
+    response["aggregates"] = aggregates
+    response["measurements"] = measurements
+
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
