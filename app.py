72e190a31ef0d53327efd3b464b4c07a6f0f3dd4
Moritz Gstuer <moritz@gstuer.com>
2025-09-02 13:20:47 +0200

    Add handling for CRC errors during runtime

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import adafruit_scd30
 from repository import Repository # Required for data persistence
 
 PERSISTENCE_WRITE_PAUSE = 10 # 60 / 10 = 6 measurements per minute
+ERROR_WRITE_PAUSE = 1
 ENABLED_SELF_CALIBRATION = True
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
@@ -13,9 +14,14 @@ scd.self_calibration_enabled = ENABLED_SELF_CALIBRATION
 
 repository = Repository()
 while True:
-    if scd.data_available:
-        # Create measurement & persist in repository
-        currentTime = time.time()
-        measurement = (currentTime, round(scd.temperature, 1), round(scd.relative_humidity, 1), round(scd.CO2, 1))
-        repository.persistMeasurement(measurement)
-    time.sleep(PERSISTENCE_WRITE_PAUSE)
+    try:
+        if scd.data_available:
+            # Create measurement & persist in repository
+            currentTime = time.time()
+            measurement = (currentTime, round(scd.temperature, 1), round(scd.relative_humidity, 1), round(scd.CO2, 1))
+            repository.persistMeasurement(measurement)
+    except RuntimeError error:
+        print("Sensor Runtime Error: ", err)
+        time.sleep(ERROR_WRITE_PAUSE)
+    finally:
+        time.sleep(PERSISTENCE_WRITE_PAUSE)
