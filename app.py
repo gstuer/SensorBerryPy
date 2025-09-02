454b16d46156bd711b42cda5cf78ec14ee088099
Moritz Gstuer <moritz@gstuer.com>
2025-09-02 12:58:38 +0200

    Enable self-calibration of SCD by default

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ import time
 import board
 import busio
 import adafruit_scd30
-# Required for data persistence
-from repository import Repository
+from repository import Repository # Required for data persistence
 
-# 60 / 10 = 6 measurements per minute
-PERSISTENCE_WRITE_PAUSE = 10
+PERSISTENCE_WRITE_PAUSE = 10 # 60 / 10 = 6 measurements per minute
+ENABLED_SELF_CALIBRATION = True
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
-scd.self_calibration_enabled = False
+scd.self_calibration_enabled = ENABLED_SELF_CALIBRATION
 
 repository = Repository()
 while True:
