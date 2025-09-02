0ca200482d4394d2d4a3bf5f96cba6a35ae6c734
Moritz Gstuer <moritz@gstuer.com>
2025-09-02 13:22:19 +0200

    Add start-up timeout to avoid I2C not ready yet

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from repository import Repository # Required for data persistence
 PERSISTENCE_WRITE_PAUSE = 10 # 60 / 10 = 6 measurements per minute
 ERROR_WRITE_PAUSE = 1
 ENABLED_SELF_CALIBRATION = True
+STARTUP_TIMEOUT = 5 # Timeout before I2C is activated in seconds
 
+time.sleep(STARTUP_TIMEOUT)
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
 scd.self_calibration_enabled = ENABLED_SELF_CALIBRATION
